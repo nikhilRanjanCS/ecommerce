62b0775d7bdfe91e5d4c010597d15282b4220850
Nikhil Ranjan <nik.ranjancool@gmail.com>
2024-11-19 14:25:43 +0530

    Deploying - modified ApiConfig.js

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "https://ecommerce-backend-production-6265.up.railway.app/"
 
 const jwt = localStorage.getItem("jwt");
 
